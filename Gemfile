4feef97b3477e69a5d7cf52635e2d14fd464e764
Darío Javier Cravero <dario@uxtemple.com>
2015-01-28 13:20:28 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'pry', group: :development
+gem 'sinatra'
---
+++ app.rb
@@ -0,0 +1,45 @@
+require 'sinatra/base'
+
+class App < Sinatra::Base
+  BASE_HOST         = ENV['BASE_HOST']    || 'http://s.uxtemple.com'
+  CODE_LENGTH       = ENV['CODE_LENGTH']  || 3
+  CODE_HEX_PATTERN  = 0xfff
+  CODE_REGEX        = /^[0-9a-z]{#{CODE_LENGTH}}$/i
+
+  set :urls, {}
+
+  get '/*' do
+    uoc = params[:splat].first
+
+    if uoc =~ CODE_REGEX
+      redirect code_to_url uoc
+    else
+      code = url_to_code uoc
+      halt 404 unless code
+      "#{BASE_HOST}/#{code}"
+    end
+  end
+
+  helpers do
+    def code_from_url lookup_url
+      match = settings.urls.find { |code, url| lookup_url == url }
+      match && match.first
+    end
+
+    def code_to_url code
+      settings.urls[code.downcase]
+    end
+
+    def url_to_code url
+      code = code_from_url(url) || random_code while code.nil? || settings.urls.include?(code)
+      settings.urls[code] = url
+      code
+    end
+
+    def random_code
+      "%0#{CODE_LENGTH}x" % (rand * CODE_HEX_PATTERN)
+    end
+  end
+
+  run!
+end
